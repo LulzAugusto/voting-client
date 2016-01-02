60e017ad5faf225b25f603293cc4fcc1aa8713c3
Luiz Augusto Crisostomo <luiz66@gmail.com>
2016-01-02 14:58:15 -0300

    Finished redux connect to current ui

---
+++ src/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router, {Route} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
+import App from './components/App';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
+
+require('./style.css');
+
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: {Sunshine: 2}
+        }
+    }
+});
+
+const routes = (
+    <Provider store={store}>
+        <Router>
+            <Route component={App}>
+                <Route path="results" component={ResultsContainer}/>
+                <Route path="/" component={VotingContainer}/>
+            </Route>
+        </Router>
+    </Provider>
+);
+
+ReactDOM.render(routes, document.getElementById('app'));
